eb2d1b4eeac2ac2ff2850c3e85353adb91e5e910
Joshua Agins <56888002+jagins@users.noreply.github.com>
2019-12-11 15:08:29 -0600

    created the component for the card that displays html elements and displays the proper properties being passed in and everything is on the webpage

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import NASAComponent from "./components/NASAComponent";
 function App() {
   return (
     <div className="App">
-      <p>
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
-      </p>
+      </p> */}
       <NASAComponent/>
     </div>
   );
--- src/components/NASACard.js
+++ src/components/NASACard.js
@@ -3,7 +3,14 @@ import React from "react";
 
 function NASACard(props)
 {
-    return null;
+    return (
+        <div className="NASACard">
+            <h1>{props.title}</h1>
+            <h2>{props.date}</h2>
+            <img src={props.imgSrc} alt={props.title}/>
+            <p>{props.explanation}</p>
+        </div>
+    );
 }   
 
 export default NASACard;
